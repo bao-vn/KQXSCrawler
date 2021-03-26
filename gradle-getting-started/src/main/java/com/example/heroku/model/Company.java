408c6a67db9a9c6bfca74d1ed050330131718f58
BaoVN <bao-vn@fanatic.vn>
2021-03-26 15:46:53 +0700

    Crawl data from html contains rss link
    - Add missing @Builder

--- gradle-getting-started/src/main/java/com/example/heroku/model/Company.java
+++ gradle-getting-started/src/main/java/com/example/heroku/model/Company.java
@@ -1,9 +1,11 @@
 package com.example.heroku.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Builder
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
