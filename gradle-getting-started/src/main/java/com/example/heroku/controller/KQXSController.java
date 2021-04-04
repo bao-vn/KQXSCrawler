67e8065b6f1e19ee2942541b52f11788f2119cbd
Võ Ngọc Bảo <1312037@student.hcmus.edu.vn>
2021-04-04 22:53:18 +0700

    Add Dockerfile

--- gradle-getting-started/src/main/java/com/example/heroku/controller/KQXSController.java
+++ gradle-getting-started/src/main/java/com/example/heroku/controller/KQXSController.java
@@ -17,6 +17,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -116,5 +117,10 @@ public class KQXSController {
         }
 
         return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello World\n";
     }
 }
